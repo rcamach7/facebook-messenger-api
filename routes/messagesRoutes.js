a01d3c9f6f3b91bb4d187435b90d2432a6c2a998
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-05-03 16:36:39 -0700

    implement messages post endpoint

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
 const messagesController = require("../controllers/messagesController");
 
+router.post("/:id", messagesController.sendMessage);
+
 module.exports = router;
